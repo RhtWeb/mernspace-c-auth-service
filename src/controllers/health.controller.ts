04e2b44160c24c252022ee249ca1fcdae4679d5c
Rohit Singh <55020650+RhtWeb@users.noreply.github.com>
2025-12-25 04:26:12 +0530

    format fix

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -3,7 +3,6 @@ import type { Request, Response } from "express";
 export const healthCheck = (_req: Request, res: Response) => {
   res.status(200).json({
     status: "ok",
-
     uptime: process.uptime(),
   });
 };
